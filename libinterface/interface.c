ec7842022fbe2429aba3d44ca5f05916f33da6bb
Sjain-dir <saksham.jain.work@gmail.com>
2023-03-30 21:17:12 +0530

    last edit

--- libinterface/interface.c
+++ libinterface/interface.c
@@ -2,8 +2,8 @@
 
 void test1(int* i) {
     printf("in test 1 :::\n");
-    printf("pointer value is : %p\n",i);
-    printf("value in that pointer is : %d\n",*i);
+    printf("i pointer value is : %p\n",i);
+    printf("i -> value in that pointer is : %d\n",*i);
 }
 
 void test2(char* string1, int *i) {
